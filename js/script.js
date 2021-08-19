9500252a3daa1c01a93d9edef23a196db8fc42c0
nikitakochunov <tonno651@gmail.com>
2021-08-19 22:50:41 +0300

    function itemActive() and header__menu with this function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 document.addEventListener('click', (event) => {
   event.preventDefault()
-  console.log(event.target);
-  if ((event.target.tagName === 'BUTTON') || (event.target.tagName === 'A')) {
-
-  }
 
 })
+
+function itemActive(menuSelector, menuItemsSelector, activeClass) {
+  const menuItems = document.querySelectorAll(menuItemsSelector)
+  const menu = document.querySelector(menuSelector)
+
+  console.log('menu', menu);
+  console.log('menu items', menuItems);
+
+  menu.addEventListener('click', (event) => {
+    console.log(event.target);
+    if ((event.target.tagName === 'A') && (!(event.target.closest('li').classList.contains(activeClass)))) {
+      console.log(1);
+      menuItems.forEach((item) => {
+        console.log(2)
+        if (item.classList.contains(activeClass)) {
+          item.classList.remove(activeClass)
+        }
+      })
+      event.target.closest('li').classList.add(activeClass)
+    }
+  })
+}
+
+itemActive('.menu__list', '.menu__item', 'menu__item--active')
